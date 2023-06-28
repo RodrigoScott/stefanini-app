cbaedef2291c9ef27a5c92da94aa3d1311121630
RodrigoScott <scott_rona1@hotmail.com>
2023-06-28 02:10:06 -0700

    feat: :art: add copy

--- lib/src/constants/copys/home_page.dart
+++ lib/src/constants/copys/home_page.dart
@@ -2,4 +2,5 @@ class HomePage {
   final String search = 'Search';
   final String noApp = 'No hay aplicaciones disponibles';
   final String category = 'Categoria';
+  final String free = 'FREE';
 }
