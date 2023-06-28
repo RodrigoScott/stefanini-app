465e82195e8baa02da698beb3b393865557ac61d
RodrigoScott <scott_rona1@hotmail.com>
2023-06-28 02:09:55 -0700

    feat: :art: implement simple dialog to show app detail

--- lib/src/pages/home_page/widgets/app_card.dart
+++ lib/src/pages/home_page/widgets/app_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:stefanini_app/src/constants/assets.dart';
 import 'package:stefanini_app/src/constants/colors.dart';
+import 'package:stefanini_app/src/constants/copys.dart';
 import 'package:stefanini_app/src/models/app_model.dart';
+import 'package:stefanini_app/src/pages/home_page/widgets/detail_card.dart';
 import 'package:stefanini_app/src/widgets/rating_bar.dart';
 
 class AppCard extends StatelessWidget {
@@ -12,54 +14,75 @@ class AppCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      width: size.width * .4,
-      height: size.height * .32,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          border: Border.all(color: AppColors.primary)),
-      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-        Container(
-            height: size.width * .35,
-            width: size.width * .35,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  Assets.avatar + appObject.avatar,
-                  fit: BoxFit.cover,
-                ))),
-        SizedBox(
-          height: 10,
-        ),
-        Expanded(
-          child: SizedBox(
-            width: size.width * .35,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    appObject.name,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    appObject.developer,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  RateBar(rate: 2),
-                  Text(
-                    '\$${appObject.cost}',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-                  )
-                ]),
+    return InkWell(
+      onTap: () => appModal(context),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        width: size.width * .4,
+        height: size.height * .32,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            border: Border.all(color: AppColors.primary)),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Container(
+              height: size.width * .35,
+              width: size.width * .35,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Hero(
+                    tag: appObject.avatar,
+                    child: Image.asset(
+                      Assets.avatar + appObject.avatar,
+                      fit: BoxFit.cover,
+                    ),
+                  ))),
+          SizedBox(
+            height: 10,
           ),
-        ),
-      ]),
+          Expanded(
+            child: SizedBox(
+              width: size.width * .35,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      appObject.name,
+                      style:
+                      TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      appObject.developer,
+                      style:
+                      TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    RateBar(rate: appObject.rank),
+                    Text(
+                      appObject.cost < 0.5
+                          ? Copys.homepage.free
+                          : '\$${appObject.cost}',
+                      style:
+                      TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                    )
+                  ]),
+            ),
+          ),
+        ]),
+      ),
     );
+  }
+
+  void appModal(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (_) =>
+            DetailCard(
+              appObject: appObject,
+            ));
   }
 }
