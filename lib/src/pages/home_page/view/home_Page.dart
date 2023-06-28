2df6b958820339d7689f75742e5916a90ee93553
RodrigoScott <scott_rona1@hotmail.com>
2023-06-28 01:03:28 -0700

    feat: :tada: implement logo widget, dropDown wwidget and grid view widget on home_page

--- lib/src/pages/home_page/view/home_Page.dart
+++ lib/src/pages/home_page/view/home_Page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:stefanini_app/src/constants/assets.dart';
+import 'package:stefanini_app/services/home_page_service.dart';
+import 'package:stefanini_app/src/constants/copys.dart';
 import 'package:stefanini_app/src/models/app_model.dart';
-import 'package:stefanini_app/src/pages/home_page/widgets/search_bar.dart';
-import 'package:stefanini_app/src/widgets/block_button_widget.dart';
+import 'package:stefanini_app/src/pages/home_page/widgets/create_logo.dart';
+import 'package:stefanini_app/src/pages/home_page/widgets/grid_view.dart';
 import 'package:stefanini_app/src/widgets/debouncer.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,10 +14,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   List<AppModel> appsFiltered = [];
   final debounce = Debouncer(milliseconds: 700);
   bool isSearch = false;
+  String searchWord = '';
+  String? category;
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,46 +30,108 @@ class _HomePageState extends State<HomePage> {
         height: size.height,
         child: Column(
           children: [
-            createLogo(size),
+            CreateLogo(
+                size: size,
+                searchFilter: searchFilter,
+                isSearch: isSearch,
+                onPress: clearData,
+                searchController: searchController),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: SizedBox(
+                width: size.width,
+                child: InputDecorator(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: category,
+                      borderRadius: BorderRadius.circular(10),
+                      underline: SizedBox(),
+                      hint: Text('Categoria'),
+                      items: [
+                        DropdownMenuItem(
+                          value: 'Finanzas',
+                          child: Text('Finanzas'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Aplicación',
+                          child: Text('Aplicación'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Entretenimiento',
+                          child: Text('Entretenimiento'),
+                        ),
+                        DropdownMenuItem(
+                          value: 'Tecnología',
+                          child: Text('Tecnología'),
+                        )
+                      ],
+                      onChanged: (cat) {
+                        if (cat != null) {
+                          category = cat;
+                          isSearch = true;
+                          setState(() {});
+                        } else {
+                          clearData();
+                        }
+                      }),
+                ),
+              ),
+            ),
+            FutureBuilder<List<AppModel>?>(
+                future: UserService().getAppList(searchWord, category),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<AppModel>?> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return const Text('Error');
+                    } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                      return GridWidget(
+                        size: size,
+                        appObject: snapshot.data != null ? snapshot.data! : [],
+                      );
+                    } else {
+                      return Expanded(
+                        child: Center(
+                            child: Text(
+                          Copys.homepage.noApp,
+                          style: TextStyle(fontSize: 20),
+                        )),
+                      );
+                    }
+                  } else {
+                    return Text('State: ${snapshot.connectionState}');
+                  }
+                }),
           ],
         ),
       ),
     );
   }
 
-  createLogo(Size size) {
-    return Container(
-            alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.only(bottom: 20),
-            color: Colors.black,
-            width: size.width,
-            height: size.height*.2,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Image.asset(Assets.logo),
-                SizedBox(height: 10,),
-                Row(mainAxisAlignment:MainAxisAlignment.center,children: [
-                  SizedBox(height: 10,),
-                  Row(children: [
-                    SizedBox(width:size.width*.7, height:size.height*0.05,child: SearchBar(searchInput: searchFilter)),
-                    SizedBox(width: 10,),
-                    SizedBox(width:size.height*.05, height:size.height*0.05,child: BlockButtonWidget(color: Color(0xfffec301), text: '', onPressed: () {  }, icon: Icons.search,)),
-                  ],)
-                ],)
-              ],
-            ),
-          );
+  clearData() {
+    searchWord = '';
+    searchController.clear();
+    category = null;
+    FocusScope.of(context).requestFocus(new FocusNode());
+    setState(() {});
   }
 
   void searchFilter(String value) {
     debounce.run(() {
       if (value.isNotEmpty) {
         isSearch = true;
-        //appsFiltered =
-            //routes.where((i) => i.nearbyExternalId!.contains(value)).toList();
+        searchWord = value;
       } else {
-        appsFiltered = [];
+        clearData();
         isSearch = false;
       }
       setState(() {});
