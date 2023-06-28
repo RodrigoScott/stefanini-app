6c54f49d5b4e898eddb24c030c0f87e8e87cc484
RodrigoScott <scott_rona1@hotmail.com>
2023-06-28 00:59:48 -0700

    feat: :art: create home page service

---
+++ lib/services/home_page_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:stefanini_app/services/data.dart';
+import 'package:stefanini_app/src/models/app_model.dart';
+
+class UserService {
+  List<AppModel>? appList;
+  List<AppModel> appListFiltered = [];
+
+  Future<List<AppModel>?> getAppList(
+      String searchWord, String? category) async {
+    try {
+      appList = (jsonDecode(dummyData) as List)
+          .map((an) => AppModel.fromJson(an))
+          .toList();
+    } catch (err) {
+      debugPrint(err.toString());
+    }
+    if (searchWord.isNotEmpty) {
+      appList!.forEach((element) {
+        if (element.name.contains(searchWord)) {
+          appListFiltered.add(element);
+        }
+      });
+      return appListFiltered;
+    }
+    if (category != null && category.isNotEmpty) {
+      appList!.forEach((element) {
+        if (element.category.contains(category)) {
+          appListFiltered.add(element);
+        }
+      });
+      return appListFiltered;
+    }
+    return appList;
+  }
+}
