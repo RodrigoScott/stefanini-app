87a5b459ddedfc142625a56bdee3f57a637234f4
RodrigoScott <scott_rona1@hotmail.com>
2023-06-27 22:13:34 -0700

    feat: :art: add custom block button

---
+++ lib/src/widgets/block_button_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class BlockButtonWidget extends StatelessWidget {
+  const BlockButtonWidget({
+    Key? key,
+    required this.color,
+    required this.text,
+    required this.onPressed,
+    this.icon,
+  }) : super(key: key);
+
+  final Color color;
+  final String text;
+  final VoidCallback onPressed;
+  final IconData? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return icon != null ? Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: color),
+      child: IconButton(
+        color: Colors.black,
+        icon:  Icon(icon),
+        onPressed: onPressed,
+      ),
+    ) : ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+          color: Colors.white,
+        ),
+      ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(color),
+        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.symmetric(
+            horizontal: 24.0, vertical: 12.0)),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
