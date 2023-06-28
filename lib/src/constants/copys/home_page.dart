d055e073b178b0b1e8991b3117e3bb8a4965aed3
RodrigoScott <scott_rona1@hotmail.com>
2023-06-28 01:21:36 -0700

    feat: :art: add copys and reformat

--- lib/src/constants/copys/home_page.dart
+++ lib/src/constants/copys/home_page.dart
@@ -1,4 +1,5 @@
 class HomePage {
   final String search = 'Search';
   final String noApp = 'No hay aplicaciones disponibles';
+  final String category = 'Categoria';
 }
--- lib/src/pages/home_page/view/home_Page.dart
+++ lib/src/pages/home_page/view/home_Page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:stefanini_app/services/home_page_service.dart';
-import 'package:stefanini_app/src/constants/copys.dart';
 import 'package:stefanini_app/src/models/app_model.dart';
 import 'package:stefanini_app/src/pages/home_page/widgets/create_logo.dart';
-import 'package:stefanini_app/src/pages/home_page/widgets/grid_view.dart';
+import 'package:stefanini_app/src/pages/home_page/widgets/dropdown_category.dart';
+import 'package:stefanini_app/src/pages/home_page/widgets/list_app.dart';
 import 'package:stefanini_app/src/widgets/debouncer.dart';
 
 class HomePage extends StatefulWidget {
@@ -36,81 +35,13 @@ class _HomePageState extends State<HomePage> {
                 isSearch: isSearch,
                 onPress: clearData,
                 searchController: searchController),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: SizedBox(
-                width: size.width,
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(10.0)),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                  ),
-                  child: DropdownButton<String>(
-                      isExpanded: true,
-                      value: category,
-                      borderRadius: BorderRadius.circular(10),
-                      underline: SizedBox(),
-                      hint: Text('Categoria'),
-                      items: [
-                        DropdownMenuItem(
-                          value: 'Finanzas',
-                          child: Text('Finanzas'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Aplicación',
-                          child: Text('Aplicación'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Entretenimiento',
-                          child: Text('Entretenimiento'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Tecnología',
-                          child: Text('Tecnología'),
-                        )
-                      ],
-                      onChanged: (cat) {
-                        if (cat != null) {
-                          category = cat;
-                          isSearch = true;
-                          setState(() {});
-                        } else {
-                          clearData();
-                        }
-                      }),
-                ),
-              ),
-            ),
-            FutureBuilder<List<AppModel>?>(
-                future: UserService().getAppList(searchWord, category),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<AppModel>?> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasError) {
-                      return const Text('Error');
-                    } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                      return GridWidget(
-                        size: size,
-                        appObject: snapshot.data != null ? snapshot.data! : [],
-                      );
-                    } else {
-                      return Expanded(
-                        child: Center(
-                            child: Text(
-                          Copys.homepage.noApp,
-                          style: TextStyle(fontSize: 20),
-                        )),
-                      );
-                    }
-                  } else {
-                    return Text('State: ${snapshot.connectionState}');
-                  }
-                }),
+            DropDownCategory(
+                size: size, categoryFilter: dropDownFilter, category: category),
+            ListApp(
+              size: size,
+              searchWord: searchWord,
+              category: category,
+            )
           ],
         ),
       ),
@@ -123,6 +54,16 @@ class _HomePageState extends State<HomePage> {
     category = null;
     FocusScope.of(context).requestFocus(new FocusNode());
     setState(() {});
+  }
+
+  void dropDownFilter(String? value) {
+    if (value != null && value != category) {
+      category = value;
+      searchController.clear();
+      searchWord = '';
+      isSearch = true;
+      setState(() {});
+    }
   }
 
   void searchFilter(String value) {
